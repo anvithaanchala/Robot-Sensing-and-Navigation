8dc29345a0952e8b83c2e192c6f75b35f79f4f84
anvitha99 <anvitha99@Ubuntu20.04.myguest.virtualbox.org>
2023-03-16 23:20:19 -0400

    LAB 3

--- build/LAB3/imu_driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anvitha99/EECE5554/src/LAB3/imu_driver
-# Build directory: /home/anvitha99/EECE5554/build/LAB3/imu_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
